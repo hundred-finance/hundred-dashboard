9e0a4aacffbf7156769f50ea35ebbd6280fc9aed
jbeanz <jbeanzsoup@gmail.com>
2022-04-20 19:13:44 -0400

    fixed rendering EpochsView, bug in chainData()

--- src/Components/Views/EpochsView.tsx
+++ src/Components/Views/EpochsView.tsx
@@ -3,7 +3,6 @@ import { useMarketContext } from "../../Types/marketContext"
 import Loading from "../Loading/loading"
 import { EpochsInfo } from "../../Types/data"
 import { useChainsContext } from "../../Types/chainsContext"
-import { getChainsEpochsInfo } from "../../Data/fetchChainData"
 
 const EpochsView = () =>{
 
@@ -17,8 +16,6 @@ const EpochsView = () =>{
         eData = chainEpochs
     }
     return(
-        { eData ? 
-        [...eData].map((network : EpochsInfo, i) => (
         
         <Row> 
                 <Col xl="7" xs="12" >
@@ -26,6 +23,7 @@ const EpochsView = () =>{
             <Table striped bordered hover variant="dark" size="sm" responsive>
                 <thead>
                     <tr>
+                        <th className="text-center align-middle">Network</th>
                         <th className="text-center align-middle">Current Epoch #</th>
                         <th className="text-center align-middle">Current Epoch Rewards</th>
                         <th className="text-center align-middle">Epoch + 1 Rewards</th>
@@ -34,25 +32,36 @@ const EpochsView = () =>{
                     </tr>
                 </thead>
                  <tbody>
-                     { network ? (
+                     { eData ? 
+                        eData.map((network : EpochsInfo) => (
+                     network ? (
                          <tr key={10000}>
+                         <td className="text-center">{network.network} </td>
                          <td className="text-center">{network.currentEpoch} </td>
                          <td className="text-center">{(network.epoch0Rewards/(10 ** 18)).toFixed(4)} </td>
                          <td className="text-center">{(network.epoch1Rewards/(10 ** 18)).toFixed(4)} </td>
                          <td className="text-center">{(network.epoch2Rewards/(10 ** 18)).toFixed(4)} </td>
                          <td className="text-center">{(network.epoch3Rewards/(10 ** 18)).toFixed(4)} </td>
                       </tr>
-                     ): <tr>
+                     ): (<tr>
                          <td colSpan={10}>
                             <Loading/>
                          </td>
-                        </tr>}
+                        </tr>
+                        )
+                         )):
+                         <tr>
+                         <td colSpan={10}>
+                            <Loading/>
+                         </td>
+                        </tr> }
                  </tbody>
                   
             </Table>
             </Col>
             </Row>
             
-        ))})}
+        )             
+}
 
 export default EpochsView
